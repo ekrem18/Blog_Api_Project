11757afe1ecf671530ff1569c42fed3eb21c29a0
Ekrem <avekremyilmazturk@gmail.com>
2023-10-05 12:58:09 +0300

    editing

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+app.use(express.json())
+
+app.all('/', (req,res) => {
+    res.send('Welcome To BlogApi')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------- */
+app.use(require('./src/errorHandler'))
+
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
