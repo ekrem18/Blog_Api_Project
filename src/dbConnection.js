f983afead2ca1df1f3e630f89a8d3361f6ee348f
Ekrem <avekremyilmazturk@gmail.com>
2023-10-05 13:11:40 +0300

    dbcon

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -2,4 +2,6 @@
 
 const mongoose = require('mongoose') //mongoose modülünü burada çağırıp mongoose değişkenine atıyoruz. atadığımız mongoose değişkeni artık tüm mongoose işlemlerinde kullanılacak. bağlanma komutu ise aşağıda
 
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect('mongodb://localhost:27017/') // başka yerden bağlanmak isteyebileceğim için elde ettiğimiz bu 'connection string'i değişken/aktif hale getirmeliyim
+    .then(()=> console.log('* DB Connected');)
+    .catch((err)=> console.log('*DB Not Connected *' , err))
