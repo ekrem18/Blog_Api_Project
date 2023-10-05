14152ebcbae934693a4f8d0884ce780bea782f6f
Ekrem <avekremyilmazturk@gmail.com>
2023-10-05 13:08:38 +0300

    monggose connect

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,1 +1,5 @@
 "use strict"
+
+const mongoose = require('mongoose') //mongoose modülünü burada çağırıp mongoose değişkenine atıyoruz. atadığımız mongoose değişkeni artık tüm mongoose işlemlerinde kullanılacak. bağlanma komutu ise aşağıda
+
+mongoose.connect('mongodb://localhost:27017/')
