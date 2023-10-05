3786c48aa19091018f4ba0cf9b695d8d466e176f
Ekrem <avekremyilmazturk@gmail.com>
2023-10-05 12:52:35 +0300

    error handleer done

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -8,9 +8,28 @@ module.exports = (err, req, res, next) => {
 
     res.status(errorStatusCode).send({ //.send() metodu stsusu ya<zdıracağımız metod. bunun içerisine object yazıyoruz ki -zorunlu değliz- kendi şablonumu oluşturayım ve okunabilirlik artsın. res.status çalışıyorsa hata var demektir bu hatayaı göreyim
         error : true,
-
-
-
+        message : err.message,
+        cause : err.cause
+        body : req.body // gönderilen json bilgi
     })
 
 }
+
+
+
+
+
+
+
+//yalın hali
+// module.exports = (err, req, res, next) => {
+
+//     const errorStatusCode = res?.errorStatusCode ?? 500
+
+//     res.status(errorStatusCode).send({
+//         error: true,
+//         message: err.message,
+//         cause: err.cause,
+//         body: req.body
+//     })
+// }
