13e5b42ae55738ad6ff6b741c501a29c689f796b
Ekrem <avekremyilmazturk@gmail.com>
2023-10-05 13:13:52 +0300

    connect

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -2,6 +2,6 @@
 
 const mongoose = require('mongoose') //mongoose modülünü burada çağırıp mongoose değişkenine atıyoruz. atadığımız mongoose değişkeni artık tüm mongoose işlemlerinde kullanılacak. bağlanma komutu ise aşağıda
 
-mongoose.connect('mongodb://localhost:27017/') // başka yerden bağlanmak isteyebileceğim için elde ettiğimiz bu 'connection string'i değişken/aktif hale getirmeliyim
-    .then(()=> console.log('* DB Connected');)
+mongoose.connect((process.env.MONGODB ?? 'mongodb://localhost:27017/')) // başka yerden bağlanmak isteyebileceğim için elde ettiğimiz bu 'connection string'i değişken/aktif hale getirmeliyim. bu localhost adresini .env'de tannımladıktan sonra burada ekliyorum
+    .then(()=> console.log('* DB Connected'))
     .catch((err)=> console.log('*DB Not Connected *' , err))
