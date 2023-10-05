25d5617b1eeebe5d6ea7cb8f0aa48eb49f912876
Ekrem <avekremyilmazturk@gmail.com>
2023-10-05 16:44:00 +0300

    module

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -2,27 +2,39 @@
 
 const mongoose = require('mongoose')
 
-const nameSchema = new mongoose.Schema({
-    //_id: auto created
+// const nameSchema = new mongoose.Schema({
+//     //_id: auto created
+//     //fieldName: String
+//     fieldName: {
+//         type: String,                         
+//         default: null, 
+//         trim: true,
+//         select: true,
+//         unique: false,                                                 //-----------> Örnek model şeması denilebilir
+//         index: false,
+//         required: [true, 'error-message'] ,
+//         validate: [function(data){return true}, 'error-message'] ,
+//         get: function(data){return true},
+//         set: function(data){return true},
+//     }
+// }, {
+//     collection: ' collectionName',
+//     timestamps: true,
+// })
 
-    //fieldName: String
-
-    fieldName: {
+const blogPost = new mongoose.Schema({
+    title:{
         type: String,
-        default: null, 
         trim: true,
-        select: true,
-        unique: false,
-        index: false,
-        required: [true, 'error-message'] ,
-        validate: [function(data){return true}, 'error-message'] ,
-        get: function(data){return true},
-        set: function(data){return true},
-        
-
-
-
+        required: true,
+    },
+    content:{
+        type: String,
+        trim: true,
+        required: true,
+    },
+    published:{
+        type:Boolean,
+        default:true,
     }
-
-
-}, {})
+}   , { collection: 'blogPost',timestamps:true })
