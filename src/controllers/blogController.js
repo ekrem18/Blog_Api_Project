cf388de6ed15922a1c6cfa38ff04591489ed169e
Ekrem <avekremyilmazturk@gmail.com>
2023-10-06 16:23:38 +0300

    upd

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,8 +2,58 @@
 
 require('express-async-errors')
 
-const { BlogCategory, BlogPost } = require('../models/blogModel')
+const { BlogPost,BlogCategory } = require('../models/blogModel')
 
+
+module.exports.BlogCategory = {
+    list: async (req, res) =>{
+
+        const data = await BlogCategory.find()   // MongoDB'de bütün kayıtlarıu getirme metodu ; find()
+        res.status(200).send({
+            error: false,
+            count: data.length,
+            result: data
+        })
+    },
+
+    create: async (req, res) =>{
+
+        const data = await BlogCategory.create(req.body) //dışarıdan göndereceğim bilgileri JSON göndercem doprudan kısadan yazıyorum diyorum
+        res.status(201).send({
+            error: false,
+            body: req.body,// gönderdiğim bilgiyi göreyim diyorum
+            result: data, //komple hepsini göreyim diyorum 
+        })
+    },
+
+    read: async (req, res) =>{
+
+        const data = await BlogCategory.findOne({ _id: req.params.postId }) 
+        res.status(200).send({
+            error: false,
+            result: data
+        })
+    },
+
+    update:  async (req, res) =>{
+
+        const data = await BlogCategory.updateOne({_id: req.params.postId}, req.body)
+        res.status(202).send({
+            error: false,
+            body: req.body,
+            result: data,
+            newData: await BlogCategory.findOne({_id: req.params.postId}) 
+        })
+    },
+
+    delete:  async (req, res) =>{
+        const data = await BlogCategory.deleteOne({_id: req.params.postId})
+        
+        res.sendStatus((data.deletedCount >= 1 ) ? 204 : 404)
+    },
+}
+
+/* ------------------------------------------------------------------------------------------------------------ */
 
 module.exports.BlogPost = {
     list: async (req, res) =>{
