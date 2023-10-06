c999f5b32f6b856019f642581a81846fa42d166c
Ekrem <avekremyilmazturk@gmail.com>
2023-10-06 16:26:01 +0300

    route upd

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -1,7 +1,19 @@
 "use strict"
 const router = require('express').Router()
-const {BlogPost} = require('../controllers/blogController')
+const {BlogPost, BlogCategory} = require('../controllers/blogController')
 
+
+
+router.route('/post')
+    .get(BlogCategory.list)
+    .post(BlogCategory.create)
+
+router.route('/post/:postId')
+    .get(BlogCategory.read)
+    .put(BlogCategory.update)
+    .delete(BlogCategory.delete)
+
+    /*--------------------------------*/
 
 router.route('/post')
     .get(BlogPost.list)
@@ -11,5 +23,7 @@ router.route('/post/:postId')
     .get(BlogPost.read)
     .put(BlogPost.update)
     .delete(BlogPost.delete)
+
+
 
 module.exports = router
