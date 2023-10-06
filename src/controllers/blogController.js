9f19fb0a947941e0920e224c0e645f5943938bea
Ekrem <avekremyilmazturk@gmail.com>
2023-10-06 13:47:21 +0300

    contorller

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -19,7 +19,12 @@ module.exports.BlogPost = {
     create: async (req, res) =>{
 
         const data = await BlogPost.insertOne(req.body) //dışarıdan göndereceğim bilgileri JSON göndercem doprudan kısadan yazıyorum diyorum
-
+        res.status(201).send({
+            error: false,
+            body: req.body,// gönderdiğim bilgiyi göreyim diyorum
+            result: data, //komple hepsini göreyim diyorum
+            
+        })
     },
 
     read: async (req, res) =>{
