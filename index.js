a101020d0d78471a3839590f958bde31437855dd
Ekrem <avekremyilmazturk@gmail.com>
2023-10-07 17:32:27 +0300

    skeleton

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
+/* -------------------------------------------------------
+------------------------------------------------------- */
 
 const express = require('express')
 const app = express()
@@ -6,27 +8,45 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 8000
 
-/*----------------------------*/
+
+
+/* ------------------------------------------------------- */
 app.use(express.json());
 
+
+
+
+/* --------Connect to MongoDB with Mongoose--------------- */
 require('./src/dbConnection')
 
 
+
+
+/* --------Home Page-------------------------------------- */
 app.all("/", (req, res) => {
   res.send("Welcome To BlogApi");
 });
 
+
+
+
+/* --------Routes----------------------------------------- */
+app.use('/user', require('./src/routes/userRoute'))
 app.use('/blog' , require('./src/routes/blogRoute'))
 
 
 
 
-
-/* ------------------------------------------------------- */
-
+/* ---------Synchronization-------------------------------- */
 // require('./src/sync')()
 
-/* ------------------------------------------------------- */
+
+
+
+/* ----------errorHandler---------------------------------- */
 app.use(require("./src/errorHandler"));
+
+
+
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
