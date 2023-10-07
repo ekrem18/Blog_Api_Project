8e448ad123dce212ce849b71f3bfd1b039d210b6
Ekrem <avekremyilmazturk@gmail.com>
2023-10-07 17:18:35 +0300

    userContorller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,3 +3,52 @@
 require('express-async-errors')
 
 const User = require('../models/userModel')
+
+
+module.exports.User = {
+    list: async (req, res) =>{
+
+        const data = await User.find()  // MongoDB'de bütün kayıtlarıu getirme metodu ; find(). 
+        res.status(200).send({
+            error: false,
+            count: data.length,
+            result: data
+        })
+    },
+
+    create: async (req, res) =>{
+
+        const data = await User.create(req.body) //dışarıdan göndereceğim bilgileri JSON göndercem doprudan kısadan yazıyorum diyorum
+        res.status(201).send({
+            error: false,
+            body: req.body,// gönderdiğim bilgiyi göreyim diyorum
+            result: data, //komple hepsini göreyim diyorum 
+        })
+    },
+
+    read: async (req, res) =>{
+
+        const data = await User.findOne({ _id: req.params.categoryId }) 
+        res.status(200).send({
+            error: false,
+            result: data
+        })
+    },
+
+    update:  async (req, res) =>{
+
+        const data = await User.updateOne({_id: req.params.categoryId}, req.body)
+        res.status(202).send({
+            error: false,
+            body: req.body,
+            result: data,
+            newData: await User.findOne({_id: req.params.categoryId}) 
+        })
+    },
+
+    delete:  async (req, res) =>{
+        const data = await User.deleteOne({_id: req.params.categoryId})
+        
+        res.sendStatus((data.deletedCount >= 1 ) ? 204 : 404)
+    },
+}
